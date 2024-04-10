d146b67d063c6b1c5cc10411c930f8d2f2f65e64
Oleg <89175955650@mail.ru>
2024-04-10 22:12:36 +0300

    next commit: img.

---
+++ img_test.py
@@ -0,0 +1,5 @@
+from PIL import Image
+
+img = Image.open('https://static-i6.move.ru/images/items/1000626493/6871243119/450x338/nd_61f1443c8151d.jpeg')
+
+img.show()
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import datetime
 import random
 from fake_headers import Headers
 from bs4 import BeautifulSoup
+from pprint import pprint
+from PIL import Image
 
 url = "https://kaluga.move.ru/kaluga/kvartiry/"
 
@@ -11,7 +13,7 @@ def test_requests():
     header = Headers().generate()
     response = requests.get(url=url, headers=header)
     response_code = response.status_code
-    print(response_code)
+    # print(response_code)
     page_text = response.text
 
     return page_text
@@ -24,13 +26,23 @@ def write_file():
 
 
 def main():
+    result_dict = {}
     num = 1
     html_text = test_requests()
     soup = BeautifulSoup(html_text, "html.parser")
-    all_title = soup.find_all("a", class_="search-item__title-link search-item__item-link")
-    for item in all_title:
-        print(num, item.text)
-        num += 1
+    all_id = soup.find_all("div", class_="search-item move-object")
+    for item_id in all_id:
+        id = item_id["data-id"]
+        img_list = []
+        # print(num, id)
+        # num += 1
+        for item_title in item_id:
+            title = item_id.find("a", class_="search-item__title-link search-item__item-link")
+            price = item_id.find("div", class_="search-item__price-values")
+            img_link = item_id.find("div", class_="brazzers-photo-wrap brazzers-preloader")
+            img_list.append(img_link["data-src"])
+            result_dict[id] = {"title": f"{title.text}", "price": (f"{price.text}").strip(), "img": img_list}
+    pprint(result_dict)
 
 
 if __name__ == "__main__":
