13d78d51acd9c399a19ccad849161358ec883183
Oleg <89175955650@mail.ru>
2024-04-16 19:15:22 +0300

    next commit.

--- main_move_kvartiry.py
+++ main_move_kvartiry.py
@@ -1,4 +1,5 @@
 import time
+import json
 
 import requests
 from fake_headers import Headers
@@ -7,6 +8,7 @@ from pprint import pprint
 
 
 def make_response():
+    # limit=10 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
     url = f"https://kaluga.move.ru/kaluga/kvartiry/vtorichnaya/?limit=100&rooms%5B%5D=-1&rooms%5B%5D=1&rooms%5B%5D=2&rooms%5B%5D=3"
     header = Headers().generate()
     response = requests.get(url=url, headers=header)
@@ -19,7 +21,16 @@ def make_response():
 
 def make_response_2(page_number):
     # url = f"https://kaluga.move.ru/kaluga/kvartiry/vtorichnaya/?page={page_number}&rooms%5B0%5D=-1&rooms%5B1%5D=1&rooms%5B2%5D=2&rooms%5B3%5D=3"
+    # &limit=100 лимит поставил 10 вместо 100 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     url = f"https://kaluga.move.ru/kaluga/kvartiry/vtorichnaya/?page={page_number}&limit=100&rooms%5B%5D=-1&rooms%5B%5D=1&rooms%5B%5D=2&rooms%5B%5D=3"
+    header = Headers().generate()
+    response = requests.get(url=url, headers=header)
+    page_text = response.text
+
+    return page_text
+
+
+def make_page_response(url):
     header = Headers().generate()
     response = requests.get(url=url, headers=header)
     page_text = response.text
@@ -63,6 +74,7 @@ def get_id_dict():
         "Text": "",
         "Photo": [],
         "Price": "",
+        "Price_mkv": "",
         "Quantity": "",
         "Price": "",
         "Old": "",
@@ -91,6 +103,7 @@ def get_id_dict():
         "Characteristics: Продавец": "",
         "Characteristics: Количество комнат": "",
         "Characteristics: ЖК": "",
+        "Characteristics: Полный адрес": "",
         "Characteristics: Город": "",
         "Characteristics: Район": "",
         "Characteristics: Улица": "",
@@ -108,6 +121,7 @@ def get_id_dict():
         "Characteristics: Количество подъездов": "",
         "Characteristics: Аварийность": "",
         "Characteristics: Обременения": "",
+        "Characteristics: инфо о доме": "",
         "Weight": "",
         "Length": "",
         "Width": "",
@@ -117,7 +131,8 @@ def get_id_dict():
         "SEO keywords": "квартиры в калуге, купить квартиру в калуге, квартиры в калуге вторичка, купить квартиру в калуге недорого",
         "FB title": "квартиры в калуге",
         "FB descr": "купить квартиру, недвижимость в калуге",
-        "Url": ""
+        "Url": "",
+        "Url_page": ""
     }
 
     return id_dict
@@ -126,14 +141,15 @@ def get_id_dict():
 def main():
     result_dict = {}
     # pages = page_count()   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    pages = 2  # сократил пагинацию, потом убрать!
-    num = 1
+    pages = 1  # сократил пагинацию, потом убрать!
+    num = 2
     for page_number in range(1, pages + 1):
         html_text = make_response()
         if page_number > 1:
             html_text = make_response_2(page_number)
         soup = BeautifulSoup(html_text, "html.parser")
         all_data = soup.find_all("div", class_="search-item move-object")
+
         for data_id in all_data:
             id_dict = get_id_dict()
             id_dict["SKU"] = data_id["data-id"]
@@ -152,6 +168,8 @@ def main():
             id_dict["Characteristics: rooms_count"] = rooms.strip()
             category = f"{(rooms.strip())[-1]}-комнатная квартира"
             id_dict["Category"] = category
+            author_type = data_id.find("li", class_="search-item__item-property search-item__provider").text
+            id_dict["Characteristics: Продавец"] = author_type.strip()
             price = data_id.find("div", class_="search-item__price-values").text
             id_dict["Price"] = price.strip()
             img_list = []
@@ -163,12 +181,51 @@ def main():
                         image = f"https:{image}"
                     img_list.append(image)
             id_dict["Photo"] = img_list
-            # print(num, data_id["data-id"], title)
+
+            page_link = data_id.find("a", class_="search-item__title-link search-item__item-link")
+            url_page = page_link.get("href")
+            if url_page[:6] != "https:":
+                url_page = f"https:{url_page}"
+            id_dict["Url_page"] = url_page
+
+            response_page = make_page_response(url_page)
+            soup = BeautifulSoup(response_page, "html.parser")
+            data_page = (soup.find("div", class_="object-page__text-block")).text.strip()
+            # print(data_page)
+            id_dict["Text"] = data_page
+
+            price_mkv = (soup.find("span", class_="block-price_second-price-value js-price-second-value")).text.strip()
+            id_dict["Price_mkv"] = price_mkv
+
+            town = (soup.find("a", class_="geo-block__geo-info_link"))
+            id_dict["Characteristics: Город"] = town.get("title")
+
+            street = (soup.find_all("a", class_="geo-block__geo-info_link"))[1]
+            id_dict["Characteristics: Улица"] = street.get("title")
+
+            type_obj = soup.find_all("div", class_="object-info__details-table_property_value")[5]
+            id_dict["Characteristics: Тип объекта"] = type_obj.text.strip()
+
+            type_nedv = soup.find_all("div", class_="object-info__details-table_property_value")[6]
+            id_dict["Characteristics: Тип недвижимости"] = type_nedv.text.strip()
+
+            zhil_pl = soup.find_all("div", class_="object-info__details-table_property_value")[8]
+            id_dict["Characteristics: Жилая площадь"] = zhil_pl.text.strip()
+
+            kuh_pl = soup.find_all("div", class_="object-info__details-table_property_value")[9]
+            id_dict["Characteristics: Площадь кухни"] = kuh_pl.text.strip()
+
+            remont = soup.find_all("div", class_="object-info__details-table_property_value")[14]
+            id_dict["Characteristics: Ремонт"] = remont.text.strip()
+
             result_dict[data_id["data-id"]] = id_dict
             num += 1
-        time.sleep(0.2)
+        # time.sleep(0.05)
         print(f"Обработана {page_number} страница.")
+
     pprint(result_dict)
+    with open('result.json', 'w', encoding="UTF-8") as fp:
+        json.dump(result_dict, fp, ensure_ascii=False)
 
 
 if __name__ == "__main__":
